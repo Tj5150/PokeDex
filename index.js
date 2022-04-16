acc0a5590d303f7b305b9c4bd5023f23a7fec077
unknown <louiskyawwin1@gmail.com>
2022-04-15 21:45:50 -0700

    charmander ability height and weight

--- index.js
+++ index.js
@@ -213,17 +213,24 @@ let charizard = `{
   ],
   "weight": 905
 }`
-
+console.log(charmander)
 // Charmander 
 charmander = JSON.parse(charmander)
 // // charmander name
 let charmanderName = (charmander.name).toUpperCase()
-// document.getElementById("charmander").innerHTML = charmanderName
-// charmander abilites
+document.getElementById("charmander").innerHTML = charmanderName
+// // charmander abilites
 console.log(charmander)
-let charmanderAbility1 = charmander.abilities[0]
-document.getElementById('')
-
+let charmanderAbility1 = charmander.abilities[0].name
+document.getElementById('ability1').innerHTML =  'Ability' + charmanderAbility1
+let charmanderAbility2 = charmander.abilities[1].name
+document.getElementById('ability2').innerHTML = 'Ability' + charmanderAbility2
+// Experience and height 
+let charmanderHeight = charmander.height
+document.getElementById('charmanderHeight').innerHTML = 'Height: ' + charmanderHeight
+let charmanderWeight = charmander.weight 
+document.getElementById('charmanderWeight').innerHTML = 'Weight: ' + charmanderWeight
+console.log(charmanderHeight)
 
 // console.log(charmanderName)
 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let charmanderName = (charmander.name).toUpperCase()
+// // document.getElementById("charmander").innerHTML = charmanderName
+// // charmander abilites
+// console.log(charmander)
+// let charmanderAbility1 = charmander.abilities[0]
+// document.getElementById('')
